aef733b4ec605f601db8ef16ab30dc8cdf5464fc
DeaDshot14 <mistry14darren@gmail.com>
2018-08-24 16:26:35 +0530

    'expense_manager'

---
+++ Easy EM/public/js/init.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAowiNCHWsAsxxQ_9u7mxPHsT_MaNrqdbc",
+    authDomain: "expense-manager-29dc8.firebaseapp.com",
+    databaseURL: "https://expense-manager-29dc8.firebaseio.com",
+    projectId: "expense-manager-29dc8",
+    storageBucket: "",
+    messagingSenderId: "764224933760"
+  };
+  firebase.initializeApp(config);
+
+ 
